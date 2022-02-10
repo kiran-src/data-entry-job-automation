f5fa26c5724b71d7031dc1dd5d7e6b872a671653
Kiran <kiransettyks@gmail.com>
2022-02-10 23:01:41 +0200

    Initial commit Create general layout for program

---
+++ GoogleForm.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+DRIVER_PATH = "C:\Program Files\selenium_chromedriver_win32\chromedriver.exe"
+FORM_URL = 'https://docs.google.com/forms/d/e/1FAIpQLSdImEVrF1ItqwTMHLVGYLb0dZB3iJEFXljySqA5YMTDcvcApQ/viewform?usp=sf_link'
+
+
+class GoogleForm:
+    def __init__(self):
+        self.s = Service(DRIVER_PATH)
+        self.driver = webdriver.Chrome(service=self.s)
+
+    def fill_entry(self):
+        pass
---
+++ main.py
@@ -0,0 +1,9 @@
+from GoogleForm import GoogleForm
+from ZilloScraper import ZilloScraper
+
+import os
+
+gf = GoogleForm()
+zs = ZilloScraper()
+
+
