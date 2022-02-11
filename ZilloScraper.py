ce5a5ceb13d8834a4a825cf49464dc854aa1a5d1
Kiran <kiransettyks@gmail.com>
2022-02-11 22:15:47 +0200

    Take advert elements and extracted essential information, saved information in list Adjusted overall layout of program

--- ZilloScraper.py
+++ ZilloScraper.py
@@ -1,13 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-ZILLO_URL = 'https://www.zillow.com/homes/for_rent/1-_beds/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22mapBounds%22%3A%7B%22west%22%3A-122.83501662207031%2C%22east%22%3A-122.03164137792969%2C%22south%22%3A37.55461173490573%2C%22north%22%3A37.995314418058285%7D%2C%22mapZoom%22%3A11%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22price%22%3A%7B%22max%22%3A712639%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A2600%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22sort%22%3A%7B%22value%22%3A%22featured%22%7D%7D%2C%22isListVisible%22%3Atrue%7D'
-
 
 class ZilloScraper:
-    def __init__(self):
+    def __init__(self, url):
         self.apartment_entries = []
-        self.site = requests.get(ZILLO_URL)
+        self.site = requests.get(url)
         self.soup = BeautifulSoup(self.site.text, 'html.parser')
 
     def scrape_apartments(self):
@@ -18,8 +16,6 @@ class ZilloScraper:
             (self.register_entry(i) for i in page_ads)
 
             more_pages = self.turn_page()
-
-
         # Loop through all available pages and perform step 1 from each one
         # In each loop, call function to remove information from element to document
         pass
@@ -27,9 +23,9 @@ class ZilloScraper:
     def register_entry(self, element):
         # Remove information from BS element and enter it into the apartment entry list
         apartment_entry = {
-            'address': "",
-            'rent': "",
-            'url': ""
+            'address': element.find(name='address', class_='list-card-addr').get_text(),
+            'rent': element.find(name='div', class_='list-card-price').get_text(),
+            'url': element.find(name='a', class_='list-card-link').get('href')
         }
         self.apartment_entries.append(apartment_entry)
 
